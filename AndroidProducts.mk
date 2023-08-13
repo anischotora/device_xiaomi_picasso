bd6d90157da8b0cf82868cba26d7706df4be27c2
anischotora <anischotora@gmail.com>
2023-08-13 22:04:50 +0600

    cherish flags

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_picasso.mk
+    $(LOCAL_DIR)/cherish_picasso.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_picasso-user \
-    lineage_picasso-userdebug \
-    lineage_picasso-eng
+    cherish_picasso-user \
+    cherish_picasso-userdebug \
+    cherish_picasso-eng
--- cherish_picasso.mk
+++ cherish_picasso.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from picasso device
 $(call inherit-product, device/xiaomi/picasso/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CherishOS stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_picasso
+PRODUCT_NAME := cherish_picasso
 PRODUCT_DEVICE := picasso
 PRODUCT_MODEL := Redmi K30 5G
 PRODUCT_BRAND := Redmi
@@ -37,3 +37,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+CHERISH_BUILD_TYPE := OFFICIAL
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer="alosrobot"
